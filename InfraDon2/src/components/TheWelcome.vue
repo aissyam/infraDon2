a9e850b3df8b46b26d0f1df7db93f60dc168335f
Aissya Mghirbi <aissya.mghirbi@gmail.com>
2025-11-06 11:56:20 +0100

    .from  and .to replication

--- InfraDon2/src/components/TheWelcome.vue
+++ InfraDon2/src/components/TheWelcome.vue
@@ -1,94 +1,150 @@
 <script setup lang="ts">
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
+const counter = ref(0)
+const increment = () => {
+  counter.value++
+}
 
-const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+import { onMounted, ref } from 'vue'
+import PouchDB from 'pouchdb'
+
+declare interface Post {
+  nom: string
+  age: number
+  ville: string
+  _id?: string
+  _rev?: string
+}
+
+// Référence à la base de données
+const storage = ref()
+// Données stockées
+const postsData = ref<Post[]>([])
+
+// Initialisation de la base de données
+const initDatabase = () => {
+  console.log('=> Connexion à la base de données')
+  const localdb = new PouchDB('ma_collection')
+  //const remotedb = new PouchDB('http://admin:cestcatastrophiiique@localhost:5984/ma_collection')
+  if (localdb) {
+    console.log('Connecté à la collection : ' + localdb?.name)
+    storage.value = localdb
+  } else {
+    console.warn('Echec lors de la connexion à la base de données')
+  }
+
+  localdb.replicate
+    .from('http://admin:cestcatastrophiiique@localhost:5984/ma_collection', {
+      live: true,
+      retry: false,
+    })
+    .on('change', (info) => {
+      console.log('Changements reçus du serveur')
+      fetchData()
+    })
+    .on('complete', () => {
+      console.log('Réplication initiale terminée')
+      fetchData()
+    })
+    .on('error', (err) => {
+      console.error('Erreur réplication pull :', err)
+    })
+
+  localdb.replicate
+    .to('http://admin:cestcatastrophiiique@localhost:5984/ma_collection', {
+      live: true,
+      retry: false,
+    })
+    .on('change', (info) => {
+      console.log('Changements reçus du serveur')
+      fetchData()
+    })
+    .on('complete', () => {
+      console.log('Réplication initiale terminée')
+      fetchData()
+    })
+    .on('error', (err) => {
+      console.error('Erreur réplication pull :', err)
+    })
+}
+
+// Récupération des données
+const fetchData = () => {
+  storage.value
+    .allDocs({
+      include_docs: true,
+      attachments: true,
+    })
+    .then((result) => {
+      postsData.value = result.rows.map((row) => row.doc)
+      console.log(postsData.value)
+      // handle result
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+
+  // TODO Récupération des données
+  // https://pouchdb.com/api.html#batch_fetch
+  // Regarder l'exemple avec function allDocs
+  // Remplir le tableau postsData avec les données récupérées
+}
+
+const createDoc = (newDoc) => {
+  storage.value
+    .post(newDoc)
+    .then((response) => {
+      console.log(response)
+      fetchData()
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
+
+const updateDoc = (doc) => {
+  const newDoc = { ...doc }
+  newDoc.ville = 'Update' + new Date().toISOString()
+  storage.value
+    .put(newDoc)
+    .then((response) => {
+      console.log(response)
+      fetchData()
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
+
+const deleteDoc = (doc) => {
+  storage.value
+    .remove(doc)
+    .then((response) => {
+      console.log(response)
+      fetchData()
+    })
+    .catch(function (err) {
+      console.log(err)
+    })
+}
+
+onMounted(() => {
+  console.log('=> Composant initialisé')
+  initDatabase()
+  fetchData()
+})
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener">Vue - Official</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">Vitest</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener">Playwright</a>.
-
-    <br />
-
-    More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
-    >.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
-    (our official Discord server), or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also follow the official
-    <a href="https://bsky.app/profile/vuejs.org" target="_blank" rel="noopener">@vuejs.org</a>
-    Bluesky account or the
-    <a href="https://x.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    X account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  <h1>Anyeonghaseyo ^^</h1>
+  <p>Counter: {{ counter }}</p>
+  <button @click="increment">+1</button>
+  <p>PostDatas</p>
+  <ul>
+    <li v-for="post in postsData" :key="post._id">
+      {{ post.nom }} - {{ post.age }} - {{ post.ville }}
+      <button @click="updateDoc(post)">Modifier</button>
+      <button @click="deleteDoc(post)">Supprimer</button>
+    </li>
+  </ul>
+  <button @click="createDoc({ nom: 'Marie', age: 22, ville: 'Séoul' })">Créer</button>
 </template>
